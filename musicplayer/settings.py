c265b93e789213eb67c1bf81cbaa397faebf759c
Dev Shakeywar <122546955+dev9310@users.noreply.github.com>
2024-12-23 15:31:39 +0530

    Update settings.py

--- musicplayer/settings.py
+++ musicplayer/settings.py
@@ -43,11 +43,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
+
 ]
 
 ROOT_URLCONF = 'musicplayer.urls'
