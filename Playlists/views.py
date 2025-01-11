4b3e39071869b20ebbaeee543aa72f8df7736bc9
Dev Shakeywar <122546955+dev9310@users.noreply.github.com>
2025-01-11 19:30:39 +0530

    commit

--- Playlists/views.py
+++ Playlists/views.py
@@ -4,12 +4,13 @@ from utils.helper import DBManager , get_supabase_client
 
 
 def Playlist(request):
-    supabase = get_supabase_client()
-    response = supabase.table("Home_song").select("*").execute()
-    songs_list = response.data  # This will be a list of dictionaries
+    # supabase = get_supabase_client()
+    # response = supabase.table("Home_song").select("*").execute()
+    # songs_list = response.data  # This will be a list of dictionaries
 
 
     
     
     
-    return render(request , 'Playlists/playlists.html', {'songs': songs_list})
+    # return render(request , 'Playlists/playlists.html', {'songs': songs_list})
+    return render(request , 'Playlists/playlists.html')
